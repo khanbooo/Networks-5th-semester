a2188de11a16e082d48493da1d5c453fe7cdf907
khanbo <sharkosergey222004@gmail.com>
2024-09-26 15:49:02 +0700

    deleted unused files

--- lab1/packet.py
+++
@@ -1,8 +0,0 @@
-class Packet:
-    def __init__(self, program_uuid=None, status=True):
-        self.uuid = program_uuid
-        self.status = status
-
-    # @property
-    # def bytes(self) -> bytes:
-    #     return bytes(self)
